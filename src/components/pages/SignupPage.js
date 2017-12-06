8fe190a9a7ef41ae366e6cef026f17d386ce1e75
jmorales2718 <john.morales@richmond.edu>
2017-12-05 20:45:36 -0500

    user signs up

--- src/components/pages/SignupPage.js
+++ src/components/pages/SignupPage.js
@@ -1,9 +1,29 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import SignupForm from "../forms/SignupForm";
+import { signup } from "../../actions/users";
 
-const SignupPage = () => (
-	<div>
-		<h1>Signup Page</h1>
-	</div>
-);
+class SignupPage extends React.Component {
+	submit = data =>
+		this.props
+			.signup(data)
+			.then(() => this.props.history.push("/dashboard"));
 
-export default SignupPage;
+	render() {
+		return (
+			<div className="ui container">
+				<SignupForm submit={this.submit} />
+			</div>
+		);
+	}
+}
+
+SignupPage.propTypes = {
+	history: PropTypes.shape({
+		push: PropTypes.func.isRequired
+	}).isRequired,
+	signup: PropTypes.func.isRequired
+};
+
+export default connect(null, { signup })(SignupPage);
